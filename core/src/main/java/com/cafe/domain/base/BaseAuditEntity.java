50dfcbc50273c999ba4b1c82c98f4ebc26d81512
ralph <hwibaski@gmail.com>
2023-09-28 15:40:57 +0900

    chore: 프로젝트 세팅
    - 멀티 모듈 적용
    - .gitignore 파일 수정
    - Docker Compose, Docker 세팅
    - BaseEntity 생성
    - 공통 ApiResponse 개발

---
+++ core/src/main/java/com/cafe/domain/base/BaseAuditEntity.java
@@ -0,0 +1,20 @@
+package com.cafe.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseAuditEntity extends BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
